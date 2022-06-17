b0819e16e90ee64cbdadbfe6f72914e8df5f33fd
Chinedu Ikechi <chineduikechi@Tech-Chinedu-2.local>
2022-06-17 15:01:54 +0100

    update middleware

--- Routes/userRouter.js
+++ Routes/userRouter.js
@@ -1,11 +1,11 @@
 require('dotenv').config();
 const express = require("express");
 const router = new express.Router();
-const {validateUser} = require('../middleware/validateUser');
+const {validateCreateUser, validateGetUser} = require('../middleware/validateUser');
 const controller = require('../controllers/user.controller');
 
-router.post("/create", validateUser, controller.CreateUser);
-router.post("/get", validateUser, controller.GetUser);
+router.post("/create", validateCreateUser, controller.CreateUser);
+router.post("/get", validateGetUser, controller.GetUser);
 router.put("/update", controller.UpdateUser);
 router.delete("/delete", controller.DeleteUser);
 
--- middleware/validateUser.js
+++ middleware/validateUser.js
@@ -1,9 +1,23 @@
-exports.validateUser = async (req, res, next) => {
+exports.validateCreateUser = async (req, res, next) => {
     const { name, password, email } = req.body;
     
     if (!name) {
         return res.status(400).send('Name is required');
     }
+
+    if (!password) {
+        return res.status(400).send('Password is required');
+    }
+
+    if (!email) {
+        return res.status(400).send('Email is required');
+    }
+
+    return next();
+};
+
+exports.validateGetUser = async (req, res, next) => {
+    const { password, email } = req.body;
 
     if (!password) {
         return res.status(400).send('Password is required');
